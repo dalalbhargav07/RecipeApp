61c9a5604551c9d59dd03b5fdaa8bf5d7ee75aa9
Bhargav Dalal <dalal.bhargav07@gmail.com>
2018-04-05 10:13:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/dalalbhargav07/recipeapp/createRecipe.java
@@ -0,0 +1,11 @@
+package com.example.dalalbhargav07.recipeapp;
+
+import java.io.Serializable;
+
+/**
+ * Created by dalalbhargav07 on 05-04-2018.
+ */
+
+public class createRecipe implements Serializable {
+
+}
