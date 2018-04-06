7e77935cce2e5a6e8984abe227a4e9c46131f6a8
Bhargav Dalal <dalal.bhargav07@gmail.com>
2018-04-06 16:47:19 -0300

    Addition of the recipe done

--- app/src/main/java/com/example/dalalbhargav07/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/dalalbhargav07/recipeapp/MainActivity.java
@@ -2,12 +2,84 @@ package com.example.dalalbhargav07.recipeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.example.dalalbhargav07.recipeapp.model.createRecipe;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText description;
+    EditText ingredients;
+    EditText instructions;
+
+    EditText rating;
+
+    Button save;
+
+    DatabaseReference recipedb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_addrecipe);
+
+        recipedb = FirebaseDatabase.getInstance().getReference("recipe");
+
+        name = findViewById(R.id.et_name);
+        description = findViewById(R.id.et_desc);
+        ingredients = findViewById(R.id.et_ingredients);
+        instructions = findViewById(R.id.et_instruction);
+        save = findViewById(R.id.savebtn);
+
+        rating = findViewById(R.id.et_rating);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addRecipe();
+            }
+        });
+    } // end of oncreate
+
+    private void addRecipe() {
+
+        String name_tv = name.getText().toString();
+        String desc = description.getText().toString();
+        String ingr = ingredients.getText().toString();
+        String instr = instructions.getText().toString();
+        String rate = rating.getText().toString();
+
+        if (!TextUtils.isEmpty(name_tv)) {
+
+            //getting a unique id using push().getKey() method
+            //it will create a unique id and we will use it as the Primary Key for our Artist
+            String id = recipedb.push().getKey();
+
+            //creating an Artist Object
+            createRecipe recipe = new createRecipe(id, name_tv,desc, ingr, instr,rate);
+
+            //Saving the Artist
+            recipedb.child(id).setValue(recipe);
+
+            //setting edittext to blank again
+            name.setText("");
+
+            //displaying a success toast
+            Toast.makeText(this, "Recipe added", Toast.LENGTH_LONG).show();
+        } else {
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/dalalbhargav07/recipeapp/model/createRecipe.java
@@ -0,0 +1,81 @@
+package com.example.dalalbhargav07.recipeapp.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by dalalbhargav07 on 05-04-2018.
+ */
+
+public class createRecipe {
+    private String recipeID;
+
+    private String recipe_name;
+
+    private String description;
+
+    private String ingredients;
+
+    private String instructions;
+
+    private String rate;
+
+
+    public createRecipe() {}
+
+    public createRecipe(String recipeID, String recipe_name, String description, String ingredients, String instructions, String rate) {
+
+        this.recipeID = recipeID;
+        this.recipe_name = recipe_name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.rate = rate;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getrecipe_name() {
+        return recipe_name;
+    }
+
+    public void setrecipe_name(String recipe_name) {
+        this.recipe_name = recipe_name;
+    }
+
+    public String getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(String recipeID) {
+        this.recipeID = recipeID;
+    }
+}
